55eb0efbd764c6dff726090e91dec72af41c6895
burako <burakokumus3@gmail.com>
2022-05-28 01:53:07 +0100

    component lifecycle and directives implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhotosComponent } from './photos/photos.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PhotosComponent
+    PhotosComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../models/post';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent implements OnInit {
+
+  pageTitle : string = 'View Posts Here!';
+  posts: Post[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.posts = [
+      {
+        id: 1,
+        title: "My first post",
+        body: "Just testing this out!",
+        vote: 1
+      },
+      {
+        id: 2,
+        title: "What's your favorite front-end framework?",
+        body: "My favorite is Angular. What do you enjoy working with?",
+        vote: 1
+      },
+      {
+        id: 3,
+        title: "ngOnInit is great",
+        body: "This lifecycle method gets called automatically upon component initialization!",
+        vote: 1
+      },
+    ];
+  }
+
+}
