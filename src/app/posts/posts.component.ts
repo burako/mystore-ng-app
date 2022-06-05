bef9e1730d4bbfc90d469cb49dc3b272baea9212
burako <burakokumus3@gmail.com>
2022-06-05 22:34:35 +0100

    dependency injection of the post service

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../models/post';
+import { PostService } from '../service/post.service';
 
 @Component({
   selector: 'app-posts',
@@ -11,29 +12,10 @@ export class PostsComponent implements OnInit {
   pageTitle : string = 'View Posts Here!';
   posts: Post[] = [];
 
-  constructor() { }
+  constructor(private postService: PostService) {}
 
   ngOnInit(): void {
-    this.posts = [
-      {
-        id: 1,
-        title: "My first post",
-        body: "Just testing this out!",
-        vote: 1
-      },
-      {
-        id: 2,
-        title: "What's your favorite front-end framework?",
-        body: "My favorite is Angular. What do you enjoy working with?",
-        vote: 1
-      },
-      {
-        id: 3,
-        title: "ngOnInit is great",
-        body: "This lifecycle method gets called automatically upon component initialization!",
-        vote: 1
-      },
-    ];
+    this.posts = this.postService.getPosts();
   }
 
   hidePost(post: Post) : void {
